acaf6da69fc2042b78bed0940134c212e729866f
zhinengge <zhinengge@gmail.com>
2015-12-12 21:57:53 +0800

    cleanad.py
    ad

---
+++ cleanad.py
@@ -0,0 +1,20 @@
+import os
+import os.path
+from shutil import copy
+
+file="D:\Program Files (x86)\Tencent\QQ\Plugin\Com.Tencent.Advertisement\Bundle.rdb"
+if os.path.exists(file):
+	os.remove(file)
+
+else:
+	print("no such file")
+ad="D:\Program Files (x86)\Tencent\QQ\Plugin\Com.Tencent.Advertisement"
+adv='D:\Program Files (x86)\Tencent\QQ\Plugin\Com.Tencent.Advertisement\_ad\Bundle.rdb'
+adn='D:\Program Files (x86)\Tencent\QQ\Plugin\Com.Tencent.Advertisement\_Nad\Bundle.rdb'
+
+if not os.path.isdir(ad):
+	pass
+else:
+	copy(adn,ad)
+print("have Nad")
+input()
